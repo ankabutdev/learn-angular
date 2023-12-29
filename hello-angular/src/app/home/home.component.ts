95741d139dabdedbd5dd044d3521dde265f48c58
ankabutdev <Ankabut571@gmail.com>
2023-12-30 03:43:30 +0500

    completed search and learn

--- hello-angular/src/app/home/home.component.ts
+++ hello-angular/src/app/home/home.component.ts
@@ -16,8 +16,19 @@ import { HousingService } from '../housing.service';
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
-
+  filteredLocationList: HousingLocation[] = [];
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
 }
